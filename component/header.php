08147434c11415a26248c4ae0371d53d454af73f
choinseong <choinseong2237@gmail.com>
2024-03-19 18:05:48 +0900

    header, footer컴포넌트화

---
+++ component/header.php
@@ -0,0 +1,17 @@
+<header>
+        <div>
+          <div class="headerdis">
+            <img src="./logo.png" alt="" class="logo">
+            <ul class="nav_ul">
+              <li class="headerli"><a href="#">information</a></li>
+              <li class="headerli"><a href="#">statistics</a></li>
+              <li class="headerli"><a href="#">reservation</a></li>
+              <li class="headerli"><a href="#">goods</a></li>
+            </ul>
+            <ul class="login_ul">
+              <li class="headerli"><a href="#">로그인</a></li>
+              <li class="headerli"><a href="#">회원가입</a></li>
+            </ul>
+          </div>
+        </div>
+</header>
